56fda8b5bae7a2bee5de9ef75169c7ebc21d3e42
cherchorek <cherchor@o2.pl>
2024-11-08 21:08:48 +0100

    Praca domowa[dzień czwarty]-zmienne i podstawowe operacje

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var imie = "Ewa";
+bool plecM = false; //plecM = true [mężczyzna], w przeciwnym wypadku kobieta
+int wiek = 29;
+
+if ((plecM != true) && (wiek < 30))
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if ((imie == "Ewa") && (wiek == 30))
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if ((plecM == true) && (wiek < 18))
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
