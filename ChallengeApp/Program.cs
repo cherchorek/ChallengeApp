5020145d1348b4d432e25ec8d1398c06fd6f8dab
cherchorek <cherchor@o2.pl>
2024-11-10 13:01:32 +0100

    zadanie domowe - program, który policzy ile jakich cyfr występuje w podanej liczbie

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,16 +1,57 @@
-﻿var name = "Ewa";
-bool sexM = false; //sexM = true [mężczyzna], w przeciwnym wypadku kobieta
-int age = 29;
+﻿int number = 4566;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
+int[] digitCount = new int[10];
 
-if ((sexM != true) && (age < 30))
+// Zliczanie wystąpień cyfr
+foreach (char c in letters)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    if (c == '0')
+    {
+        digitCount[0]++;
+    }
+    else if (c == '1')
+    {
+        digitCount[1]++;
+    }
+    else if (c == '2')
+    {
+        digitCount[2]++;
+    }
+    else if (c == '3')
+    {
+        digitCount[3]++;
+    }
+    else if (c == '4')
+    {
+        digitCount[4]++;
+    }
+    else if (c == '5')
+    {
+        digitCount[5]++;
+    }
+    else if (c == '6')
+    {
+        digitCount[6]++;
+    }
+    else if (c == '7')
+    {
+        digitCount[7]++;
+    }
+    else if (c == '8')
+    {
+        digitCount[8]++;
+    }
+    else if (c == '9')
+    {
+        digitCount[9]++;
+    }
+
 }
-else if ((name == "Ewa") && (age == 30))
+
+// Wyświetlanie wyników
+Console.WriteLine($"Wyniki dla liczby: {number}");
+for (int i = 0; i < digitCount.Length; i++)
 {
-    Console.WriteLine("Ewa, lat 30");
-}
-else if ((sexM == true) && (age < 18))
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
+    Console.WriteLine($"Cyfra {i} => {digitCount[i]}");
 }
